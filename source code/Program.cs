9aa3c7c4d6af448e8007c97c0ba3e5323ccec214
Mahmoud-AbdelFatah <abdelfatahm3@gmail.com>
2016-04-23 23:14:57 +0200

    source code

---
+++ source code/Program.cs
@@ -0,0 +1,426 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DS_project
+{
+
+    public class Node<T>
+    {
+        public T Nodeval;
+        public Node<T> right;
+        public Node<T> left;
+        public Node<T> parent;
+        public Form NodeForm;
+        public int x, y;
+        public System.Drawing.Graphics graphNode;
+        public System.Drawing.Rectangle rectangle;
+
+        StringFormat strformat;  //of the text      
+        Font font; //font of the text
+        SolidBrush brush;//the color of the text
+        Pen pen;
+        public void unfill()
+        {
+            pen = new Pen(brush, 5);
+            graphNode.DrawEllipse(pen, rectangle);
+        }
+        public void fill()
+        {
+            pen = new Pen(System.Drawing.Brushes.Black, 5);
+            graphNode.DrawEllipse(pen, rectangle);
+        }
+        public void DrawNode(string n, int x_pos, int y_pos)
+        {
+            pen = new Pen(brush, 5);
+            graphNode = NodeForm.CreateGraphics();
+            rectangle = new System.Drawing.Rectangle(x_pos, y_pos, 50, 50);
+
+            graphNode.DrawString(n, font, brush, rectangle, strformat);
+            graphNode.DrawEllipse(pen, rectangle);
+        }
+
+        public void removeNode()
+        {
+            //graphNode.Clear(Color.Black);
+            NodeForm.Invalidate(rectangle);
+        }
+        public Node(T v, Form form)
+        {
+            NodeForm = form;
+            x = form.Width / 2 - 33;
+            y = 50;
+            Nodeval = v;
+            right = null;
+            left = null;
+
+            font = new Font("Arial", 8); //the font of the text
+            brush = new SolidBrush(Color.White); //the color of the text
+
+            strformat = new StringFormat();
+            strformat.Alignment = StringAlignment.Center;
+            strformat.LineAlignment = StringAlignment.Center;
+
+            DrawNode(Nodeval.ToString(), x, y);
+            //DrawArrow();
+        }
+
+        public Node(T v, int x_pos, int y_pos, Node<T> parentNode, Form form)
+        {
+            NodeForm = form;
+            parent = parentNode;
+            Nodeval = v;
+            right = null;
+            left = null;
+            x = x_pos;
+            y = y_pos;
+
+            font = new Font("Arial", 8); //the text font
+            brush = new SolidBrush(Color.White); //the color of the text            
+
+            strformat = new StringFormat();
+            strformat.Alignment = StringAlignment.Center;
+            strformat.LineAlignment = StringAlignment.Center;
+
+            DrawNode(Nodeval.ToString(), x, y);
+
+            DrawArrow();
+        }
+
+
+        public void DrawArrow()
+        {
+            //arrow
+            Pen p = new Pen(Brushes.Black, 4);
+            p.StartCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
+            if (Comparer<T>.Default.Compare(Nodeval, parent.Nodeval) > 0) //right
+            {
+                graphNode.DrawLine(p, x + 25, y, parent.x + 38, parent.y + 45);
+            }
+            else if (Comparer<T>.Default.Compare(Nodeval, parent.Nodeval) < 0) //left
+            {
+                graphNode.DrawLine(p, x + 25, y, parent.x + 12, parent.y + 45);
+            }
+        }
+    }
+
+    public class BST<T>
+    {
+        public Node<T> root;
+        public int size;
+        public Form bstForm;
+
+        public BST(Form f)
+        {
+            bstForm = f;
+            size = 0;
+            root = null;
+        }
+
+        public Node<T> findParent(T val)
+        {//tested
+            Node<T> temp1 = root;
+            Node<T> temp2 = null;
+            while (temp1 != null)
+            {
+
+                if (temp1.Nodeval.Equals(val))
+                    break;
+
+                temp2 = temp1;
+                if (Comparer<T>.Default.Compare(temp1.Nodeval, val) > 0)
+                {
+                    temp1 = temp1.left;
+                }
+                else
+                {
+                    temp1 = temp1.right;
+                }
+            }
+            return temp2;
+        }
+        public double ParentLevel(T val)
+        {
+            Node<T> temp1 = root;
+            Node<T> temp2 = null;
+            double level = -1;
+            while (temp1 != null)
+            {
+
+                if (temp1.Nodeval.Equals(val))
+                    break;
+
+                temp2 = temp1;
+                if (Comparer<T>.Default.Compare(temp1.Nodeval, val) > 0)
+                {
+                    temp1 = temp1.left;
+                    level++;
+                }
+                else
+                {
+                    temp1 = temp1.right;
+                    level++;
+                }
+            }
+            return level;
+        }
+        public void append(T val, bool animate)
+        {//tested
+            if (size == 0)
+            {
+                root = new Node<T>(val, bstForm);
+            }
+            else
+            {
+                Node<T> par = findParent(val);
+                if (contain(val, animate) == true)
+                {
+                    //MessageBox.Show("the element is alrady exist ! append()");
+                    return;
+                }
+                else if (Comparer<T>.Default.Compare(findParent(val).Nodeval, val) > 0) //findParent(val).Nodeval > val
+                {
+                    //the new value < parent val               
+                    double xpos = par.x - (bstForm.Width / 4) / Math.Pow(2.0, ParentLevel(val));
+
+                    par.left = new Node<T>(val, (int)xpos, par.y + 90, par, bstForm);
+                }
+                else if (Comparer<T>.Default.Compare(findParent(val).Nodeval, val) < 0)
+                {
+                    //the new val > parent val
+                    double xpos = par.x + (bstForm.Width / 4) / Math.Pow(2.0, ParentLevel(val));
+                    par.right = new Node<T>(val, (int)xpos, par.y + 90, par, bstForm);
+                }
+            }
+
+            size++;
+        }
+        public bool contain(T val, bool animate)
+        {//tested
+            //returns true if the value is already exist in the BST
+            Node<T> temp = root;
+            while (temp != null)
+            {
+                if (temp.Nodeval.Equals(val))
+                {
+                    if (animate)
+                    {
+                        temp.fill();
+                        System.Threading.Thread.Sleep(500);
+                        temp.unfill();
+                        System.Threading.Thread.Sleep(500);
+                        temp.fill();
+                        System.Threading.Thread.Sleep(500);
+                        temp.unfill();
+                        System.Threading.Thread.Sleep(500);
+                        temp.fill();
+                        System.Threading.Thread.Sleep(500);
+                        temp.unfill();
+                    }
+                    return true;
+                }
+                else if (Comparer<T>.Default.Compare(temp.Nodeval, val) < 0)
+                {
+                    if (animate)
+                    {
+                        temp.fill();
+                        System.Threading.Thread.Sleep(500);
+                        temp.unfill();
+                    }
+                    temp = temp.right;
+
+                }
+                else
+                {
+                    if (animate)
+                    {
+                        temp.fill();
+                        System.Threading.Thread.Sleep(500);
+                        temp.unfill();
+                    }
+                    temp = temp.left;
+                }
+
+            }
+            return false;
+        }
+
+        public void traverce(int c)
+        {
+            if (c == 1)
+                inOrder(root);
+            else if (c == 2)
+                preOrder(root);
+            else
+                postOrder(root);
+        }
+        public void inOrder(Node<T> start)
+        {//tested
+            if (start != null)
+            {
+                inOrder(start.left);
+                MessageBox.Show(start.Nodeval.ToString());
+                inOrder(start.right);
+            }
+        }
+        public void preOrder(Node<T> start)
+        {//tested
+            if (start != null)
+            {
+                MessageBox.Show(start.Nodeval.ToString());
+                preOrder(start.left);
+                preOrder(start.right);
+            }
+        }
+        public void postOrder(Node<T> start)
+        {//tested
+            if (start != null)
+            {
+                postOrder(start.left);
+                postOrder(start.right);
+                MessageBox.Show(start.Nodeval.ToString());
+            }
+        }
+
+        public Node<T> findNode(T val)
+        {//returns null if the value is not there
+            //tested
+            Node<T> temp = root;
+            while (temp != null)
+            {
+                if (temp.Nodeval.Equals(val))
+                    break;
+
+                if (Comparer<T>.Default.Compare(temp.Nodeval, val) > 0)
+                {
+                    temp = temp.left;
+                }
+                else
+                {
+                    temp = temp.right;
+                }
+            }
+            return temp;
+        }
+        public void Delete(T val)
+        {//tested
+            Node<T> temp = findNode(val);
+            if (temp == null)
+            {
+                MessageBox.Show("the value you want to delete is not exist ! Delete()");
+                return;
+            }
+            else
+            {
+                if (temp.left == null && temp.right == null)
+                {
+                    if (temp == root)
+                        root = null;
+                    else
+                    {
+                        Node<T> parent = findParent(val);
+                        if (Comparer<T>.Default.Compare(parent.Nodeval, val) > 0)
+                        {
+                            parent.left = null;
+                        }
+                        else
+                        {
+                            parent.right = null;
+                        }
+                    }
+                }
+                else if (temp.left == null && temp.right != null)
+                {
+                    Node<T> parent = findParent(val);
+                    if (temp == root)
+                        root = temp.right;
+                    else
+                    {
+                        if (Comparer<T>.Default.Compare(parent.Nodeval, val) > 0)
+                            parent.left = temp.right;
+                        else
+                            parent.right = temp.right;
+                    }
+                }
+                else if (temp.left != null && temp.right == null)
+                {
+                    Node<T> parent = findParent(val);
+                    if (temp == root)
+                        root = temp.left;
+                    else
+                    {
+                        if (Comparer<T>.Default.Compare(parent.Nodeval, val) > 0)
+                            parent.left = temp.left;
+                        else
+                            parent.right = temp.left;
+                    }
+                }
+                else
+                {
+                    Node<T> minNode = findMin(temp.right);
+                    Node<T> parent = findParent(minNode.Nodeval);
+                    temp.Nodeval = minNode.Nodeval;
+                    if (parent == temp)
+                    {
+                        parent.right = minNode.right;
+                    }
+                    else
+                    {
+                        parent.left = minNode.right;
+                    }
+                }
+            }
+            size--;
+        }
+
+        public Node<T> findMin(Node<T> start)
+        {
+            Node<T> temp = start;
+            while (temp.left != null)
+                temp = temp.left;
+            return temp;
+        }
+    }
+    
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+                Choose f1 = new Choose() ;
+                Application.Run(f1);
+
+            string c = f1.cho;
+            
+
+            if (c == "int")
+            {
+                Application.Run(new IntBST());
+                f1.Close();
+            }
+            else if (c == "float")
+            {
+                Application.Run(new FloatBST());
+                f1.Close();
+            }
+            else
+            {
+                Application.Run(new StringBST());
+                f1.Close();
+            }
+                
+        }
+    }
+}
---
+++ source code/floatBST.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace DS_project
+{
+    partial class FloatBST
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FloatBST));
+            this.Refresh = new System.Windows.Forms.Button();
+            this.Merge = new System.Windows.Forms.Button();
+            this.Seacrh = new System.Windows.Forms.Button();
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.Open = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.SaveAs = new System.Windows.Forms.Button();
+            this.Undo = new System.Windows.Forms.Button();
+            this.Restart = new System.Windows.Forms.Button();
+            this.DelBox = new System.Windows.Forms.TextBox();
+            this.Delete = new System.Windows.Forms.Button();
+            this.AppBox = new System.Windows.Forms.TextBox();
+            this.Append = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Refresh
+            // 
+            this.Refresh.Location = new System.Drawing.Point(1194, 43);
+            this.Refresh.Name = "Refresh";
+            this.Refresh.Size = new System.Drawing.Size(75, 23);
+            this.Refresh.TabIndex = 35;
+            this.Refresh.Text = "Refresh";
+            this.Refresh.UseVisualStyleBackColor = true;
+            this.Refresh.Click += new System.EventHandler(this.Refresh_Click);
+            // 
+            // Merge
+            // 
+            this.Merge.Location = new System.Drawing.Point(1194, 14);
+            this.Merge.Name = "Merge";
+            this.Merge.Size = new System.Drawing.Size(75, 23);
+            this.Merge.TabIndex = 34;
+            this.Merge.Text = "Merge";
+            this.Merge.UseVisualStyleBackColor = true;
+            this.Merge.Click += new System.EventHandler(this.Merge_Click);
+            // 
+            // Seacrh
+            // 
+            this.Seacrh.Location = new System.Drawing.Point(8, 70);
+            this.Seacrh.Name = "Seacrh";
+            this.Seacrh.Size = new System.Drawing.Size(75, 23);
+            this.Seacrh.TabIndex = 33;
+            this.Seacrh.Text = "Search";
+            this.Seacrh.UseVisualStyleBackColor = true;
+            this.Seacrh.Click += new System.EventHandler(this.Search_Click);
+            // 
+            // searchBox
+            // 
+            this.searchBox.Location = new System.Drawing.Point(90, 71);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(88, 20);
+            this.searchBox.TabIndex = 32;
+            this.searchBox.Text = " Search ...";
+            this.searchBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.searchBox_MouseClick);
+            this.searchBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.searchBox_KeyDown);
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(1275, 14);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(75, 23);
+            this.Open.TabIndex = 31;
+            this.Open.Text = "Open";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(1275, 40);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 30;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // SaveAs
+            // 
+            this.SaveAs.Location = new System.Drawing.Point(1275, 68);
+            this.SaveAs.Name = "SaveAs";
+            this.SaveAs.Size = new System.Drawing.Size(75, 23);
+            this.SaveAs.TabIndex = 29;
+            this.SaveAs.Text = "Save as";
+            this.SaveAs.UseVisualStyleBackColor = true;
+            this.SaveAs.Click += new System.EventHandler(this.SaveAs_Click);
+            // 
+            // Undo
+            // 
+            this.Undo.Location = new System.Drawing.Point(8, 99);
+            this.Undo.Name = "Undo";
+            this.Undo.Size = new System.Drawing.Size(75, 23);
+            this.Undo.TabIndex = 28;
+            this.Undo.Text = "Undo";
+            this.Undo.UseVisualStyleBackColor = true;
+            this.Undo.Click += new System.EventHandler(this.Undo_Click);
+            // 
+            // Restart
+            // 
+            this.Restart.Location = new System.Drawing.Point(8, 128);
+            this.Restart.Name = "Restart";
+            this.Restart.Size = new System.Drawing.Size(75, 23);
+            this.Restart.TabIndex = 27;
+            this.Restart.Text = "Restart";
+            this.Restart.UseVisualStyleBackColor = true;
+            this.Restart.Click += new System.EventHandler(this.Restart_Click);
+            // 
+            // DelBox
+            // 
+            this.DelBox.Location = new System.Drawing.Point(90, 43);
+            this.DelBox.Name = "DelBox";
+            this.DelBox.Size = new System.Drawing.Size(88, 20);
+            this.DelBox.TabIndex = 26;
+            this.DelBox.Text = " Delete from BST";
+            this.DelBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.DelBox_MouseClick);
+            this.DelBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.DelBox_KeyDown);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(8, 41);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 23);
+            this.Delete.TabIndex = 25;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // AppBox
+            // 
+            this.AppBox.Location = new System.Drawing.Point(90, 14);
+            this.AppBox.Name = "AppBox";
+            this.AppBox.Size = new System.Drawing.Size(88, 20);
+            this.AppBox.TabIndex = 24;
+            this.AppBox.Text = " append to BST";
+            this.AppBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.AppBox_MouseClick);
+            this.AppBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.AppBox_KeyDown);
+            // 
+            // Append
+            // 
+            this.Append.Location = new System.Drawing.Point(8, 12);
+            this.Append.Name = "Append";
+            this.Append.Size = new System.Drawing.Size(75, 23);
+            this.Append.TabIndex = 23;
+            this.Append.Text = "append";
+            this.Append.UseVisualStyleBackColor = true;
+            this.Append.Click += new System.EventHandler(this.Append_Click);
+            // 
+            // FloatBST
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.ClientSize = new System.Drawing.Size(1362, 416);
+            this.Controls.Add(this.Refresh);
+            this.Controls.Add(this.Merge);
+            this.Controls.Add(this.Seacrh);
+            this.Controls.Add(this.searchBox);
+            this.Controls.Add(this.Open);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.SaveAs);
+            this.Controls.Add(this.Undo);
+            this.Controls.Add(this.Restart);
+            this.Controls.Add(this.DelBox);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.AppBox);
+            this.Controls.Add(this.Append);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FloatBST";
+            this.Text = "FloatBST";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Refresh;
+        private System.Windows.Forms.Button Merge;
+        private System.Windows.Forms.Button Seacrh;
+        private System.Windows.Forms.TextBox searchBox;
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button SaveAs;
+        private System.Windows.Forms.Button Undo;
+        private System.Windows.Forms.Button Restart;
+        private System.Windows.Forms.TextBox DelBox;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.TextBox AppBox;
+        private System.Windows.Forms.Button Append;
+
+    }
+}
